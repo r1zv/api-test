20b1d1dd897900daebc225213f60a9f933f2b2b1
r1zv <rizv06@gmail.com>
2019-01-30 01:33:39 -0500

    yay we got some data :D

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
-console.log('hi :D');
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Summary extends Component {
+  constructor(props) {
+    super(props) 
+      this.state = {
+        data: null,
+        year:"2013",
+        attendance:null,
+        oGPA:null
+       }    
+  this.fetchData = this.fetchData.bind(this);
+  }
+
+
+  componentDidMount() {
+  this.fetchData();
+  }
+
+  fetchData() {
+  const { data } = axios.get(
+    'http://apitest.sertifi.net/api/Students'
+  ).then(res => {
+    const data = res.data;
+    this.setState({data});
+  })
+  console.log(data);
+    debugger;
+  this.setState({
+    data:data
+  })
+}
+  render() {
+    var greeting = "Student Summary";
+    return (
+      <div>
+        <h1>{greeting}</h1>
+        <ul>
+        <li>Year: {this.state.year}</li>
+        <li>Attendance: {this.state.attendace}</li>
+        <li>Overall GPA: {this.state.oGPA}</li>
+        </ul>
+      </div>
+      )
+  }
+}
+
+
+export default Summary;
