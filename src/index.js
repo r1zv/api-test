496b407985a3c2ad42bd4b14f48000c1b2afe433
r1zv <rizv06@gmail.com>
2019-01-29 20:10:52 -0500

    react and hot reloader init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'API TEST';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
