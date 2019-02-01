78a8df901c3466f74a10cfc7997797df4a6f9db9
r1zv <rizv06@gmail.com>
2019-02-01 02:56:26 -0500

    add logic for finding current GPA per student in 2013

--- src/components/students.component.js
+++ src/components/students.component.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
 export default class Students extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  findGPA() {
+    let data = this.props.students;
+    let newData = [];
+    data.map(function(elem) {
+      elem.SecondYear = elem.StartYear + 1;
+      elem.ThirdYear = elem.StartYear + 2;
+    });
+    data.map(function(elem) {
+      let StartYear = elem.StartYear;
+      let SecondYear = elem.SecondYear;
+      let ThirdYear = elem.ThirdYear;
+      let EndYear = elem.EndYear;
+      elem.newGPARecord = [
+        {
+          [StartYear]: elem.GPARecord[0],
+          [SecondYear]: elem.GPARecord[1],
+          [ThirdYear]: elem.GPARecord[2],
+          [EndYear]: elem.GPARecord[3]
+        }
+      ];
+    });
+    this.setState({ data });
+  }
+
   render() {
-    const studentList = this.props.students;
+    const studentList = this.state.data;
     const displayStudents = studentList.map(d => (
-      <li key={d.Id}>Name: {d.Name}</li>
+      <li key={d.Id}>
+        Name: {d.Name} <br />
+        2013 GPA: {d.newGPARecord["0"][2013]}{" "}
+      </li>
     ));
     return (
       <div>
@@ -13,6 +47,6 @@ export default class Students extends Component {
     );
   }
   componentDidMount() {
-    console.log(this);
+    this.findGPA();
   }
 }
